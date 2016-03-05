1a9b230001bd60c8176f10096d2bf10a47c20828
Dylan Thomas <kdylanthomas@gmail.com>
2016-03-05 14:51:45 -0600

    MVP + minor refactoring

--- js/weapons.js
+++ js/weapons.js
@@ -8,9 +8,6 @@ function Weapon (name) {
 	this.damage = 0;
 }
 
-// rewrite specific weapons into prototypes??
-
-// set laserGun damage to 5-8
 function LaserGun () {
 	this.maxDamage = 8;
 	this.minDamage = 5;
@@ -18,7 +15,7 @@ function LaserGun () {
 
 LaserGun.prototype = new Weapon("Laser Gun");
 
-// set electricBoomerang damage to 2-9
+
 function ElectricBoomerang () {
 	this.maxDamage = 9;
 	this.minDamage = 2;
@@ -27,16 +24,14 @@ function ElectricBoomerang () {
 ElectricBoomerang.prototype = new Weapon("Electric Boomerang");
 
 
-// set electricBoomerang damage to 2-8
 function ThrowingStars () {
 	this.maxDamage = 8;
 	this.minDamage = 2;
 }
 
-ThrowingStars.prototype = new Weapon("Carbon Fiber Throwing Stars");
+ThrowingStars.prototype = new Weapon("Throwing Stars");
 
 
-// set flamethrower damage to 5-11
 function Flamethrower () {
 	this.maxDamage = 11;
 	this.minDamage = 5;
@@ -44,7 +39,7 @@ function Flamethrower () {
 
 Flamethrower.prototype = new Weapon("Flamethrower");
 
-// set giant magnet damage to 8-10
+
 function GiantMagnet () {
 this.maxDamage = 10;
 this.minDamage = 8;
@@ -52,7 +47,6 @@ this.minDamage = 8;
 
 GiantMagnet.prototype = new Weapon("Giant Magnet");
 
-// set mayo damage to 3-15
 
 function Mayonnaise () {
 	this.maxDamage = 16;
